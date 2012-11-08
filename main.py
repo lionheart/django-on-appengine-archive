4eafe790672252986861a9f0e4d2a926747bfdf0
Gabriel Pugliese <gabriel@siteapps.com>
2012-11-08 16:23:37 -0200

    Fixed a bug where using use_library does not work in production

--- main.py
+++ main.py
@@ -2,20 +2,12 @@ import logging
 import os
 import sys
 
-from google.appengine.dist import use_library
-
-for k in [k for k in sys.modules if k.startswith('django')]:
-    del sys.modules[k]
-
 # You can change this if you'd like.
-use_library('django', '1.4')
+#use_library('django', '1.4')
 
 # Must set this env var before importing any part of Django
 # 'project' is the name of the project created with django-admin.py
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-# Google App Engine imports.
-from google.appengine.ext.webapp import util
 
 import django.core.handlers.wsgi
 import django.core.signals
